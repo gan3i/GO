1eef6bd44bec08ef2058a1bec9a88a68c749eb64
gan3i <gani468@gmail.com>
2021-04-29 09:10:36 +0530

    secret changes

--- Cards/exotel/exo_call_agent.go
+++ Cards/exotel/exo_call_agent.go
@@ -10,15 +10,15 @@ import (
 )
 
 func main() {
-	apiToken := "a181147c613dcd3e82908899084e5d39214e4a345eebae54"
-	exotelSid := "ekagga2"
-	apiKey := "741cbdd276bd1d0d81b3e3694c3205bbbf9d93794f96bb21"
+	apiToken := ""
+	exotelSid := ""
+	apiKey := ""
 
 	baseURL := fmt.Sprintf("https://%s:%s@api.exotel.com/v1/accounts/%s/Calls/connect.json", apiKey, apiToken, exotelSid)
 
 	callData := url.Values{}
-	callData.Set("To", "09686419568")
-	callData.Set("From", "09686419568")
+	callData.Set("To", "")
+	callData.Set("From", "")
 	callData.Set("CallerType", "trans")
 
 	callDataReader := strings.NewReader(callData.Encode())
